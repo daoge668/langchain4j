bed2e21161cf27d7ce39935500a9a297a704f892
daoge <2318891066@qq.com>
2025-10-03 19:34:10 +0800

    将对话信息缓存在redis

--- src/main/java/com/itheima/langchain4j/Config/CommonConfig.java
+++ src/main/java/com/itheima/langchain4j/Config/CommonConfig.java
@@ -6,12 +6,15 @@ import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.service.AiServices;
+import dev.langchain4j.store.memory.chat.ChatMemoryStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommonConfig {
+    @Autowired
+    private ChatMemoryStore redisChatMemoryStore;
     @Autowired
     private OpenAiChatModel model;
 //    @Bean
@@ -30,6 +33,7 @@ public class CommonConfig {
                         .builder()
                         .id(memoryId)//设置唯一标识符为传入的memoryId，这样可以区分不同的对话会话
                         .maxMessages(20)//设置消息窗口的最大容量为20条，限制对话历史的长度
+                        .chatMemoryStore(redisChatMemoryStore)
                         .build();//调用构建器的build()方法，完成MessageWindowChatMemory对象的创建
             }
         };
